fe669b1ca16313023636340dc5c4a3fd023e47d5
Hironori Akaishi <ah.gm3622@gmail.com>
2019-03-27 23:53:17 +0900

    update: table

--- src/be/db/migrations/20190323185138_createTweet.sql
+++ src/be/db/migrations/20190323185138_createTweet.sql
@@ -2,10 +2,12 @@
 -- +goose Up
 -- SQL in section 'Up' is executed when this migration is applied
 CREATE TABLE tweets (
-  id BIGINT NOT NULL AUTO_INCREMENT,
+  id SERIAL,
   text TEXT NOT NULL,
-  PRIMARY KEY(id),
-)
+  created_at timestamp not null default current_timestamp,
+  update_at timestamp not null default current_timestamp on update current_timestamp,
+  CONSTRAINT pk_tweets PRIMARY KEY (id)
+);
 
 -- +goose Down
 -- SQL section 'Down' is executed when this migration is rolled back
